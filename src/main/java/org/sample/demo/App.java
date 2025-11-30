35f770b1e01aea7e5f8591064429168e91f2106a
sravan277 <155646562+sravan277@users.noreply.github.com>
2025-12-01 00:10:30 +0530

    Update App.java

--- src/main/java/org/sample/demo/App.java
+++ src/main/java/org/sample/demo/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("stfu");
+        System.out.println("smd");
 
     }
 }
